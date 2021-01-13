58158b41de4e00f1a94670468adf0f5fcc75d132
sabrina-fontana <sabfon@hotmail.it>
2021-01-13 16:51:37 +0100

    modifica funzioni

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
 // FUNZIONI
-function slideLeft(activeImage, firstImage, lastImage, activeClass) {
+function slideLeft() {
   // se la prima immagine è active, ricomincio dall'ultima
-  if ($(firstImage).hasClass(activeClass)) {
-    $(firstImage).removeClass(activeClass);
-    $(lastImage).addClass(activeClass);
+  if ($('img.first').hasClass('active')) {
+    $('img.first').removeClass('active');
+    $('img.last').addClass('active');
     // altrimenti vai a quella precedente
   } else {
-   $(activeImage).removeClass(activeClass).prev().addClass(activeClass);
+   $('img.active').removeClass('active').prev().addClass('active');
  }
 }
 
-function slideRight(activeImage, firstImage, lastImage, activeClass) {
+function slideRight() {
   // se l'ultima immagine è active, ricomincia dalla prima
-  if ($(lastImage).hasClass(activeClass)) {
-    $(lastImage).removeClass(activeClass);
-    $(firstImage).addClass(activeClass);
+  if ($('img.last').hasClass('active')) {
+    $('img.last').removeClass('active');
+    $('img.first').addClass('active');
   // atrimenti vai a quella successiva
   } else {
-   $(activeImage).removeClass(activeClass).next().addClass(activeClass);
+   $('img.active').removeClass('active').next().addClass('active');
  }
 }
-
 
 // <- clicco sulla freccia a sinistra (torno indietro)
 $('.fa-angle-left').click(function() {
-  slideLeft('img.active', 'img.first', 'img.last', 'active');
+  slideLeft();
  }
 );
 
 // -> clicco sulla freccia a destra (vado avanti)
 $('.fa-angle-right').click(function() {
-  slideRight('img.active', 'img.first', 'img.last', 'active');
+  slideRight();
 });
 
 // per scorrere le immagini permettere anche l’uso delle frecce sinistra (37) e destra (39) della tastiera
 $(document).keydown(function(k) {
   if(k.keyCode == 37) { // left
-    slideLeft('img.active', 'img.first', 'img.last', 'active');
+    slideLeft();
   }
   else if(k.keyCode == 39) { // right
-    slideRight('img.active', 'img.first', 'img.last', 'active');
+    slideRight();
   }
 });
