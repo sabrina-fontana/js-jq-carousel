ba7cee04f04dd8957e36a897f5bd06075279ca0a
sabrina-fontana <sabfon@hotmail.it>
2021-01-13 16:30:11 +0100

    aggiunta funzioni

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
+// FUNZIONI
+function slideLeft(activeImage, firstImage, lastImage, activeClass) {
+  // se la prima immagine è active, ricomincio dall'ultima
+  if ($(firstImage).hasClass(activeClass)) {
+    $(firstImage).removeClass(activeClass);
+    $(lastImage).addClass(activeClass);
+    // altrimenti vai a quella precedente
+  } else {
+   $(activeImage).removeClass(activeClass).prev().addClass(activeClass);
+ }
+}
+
+function slideRight(activeImage, firstImage, lastImage, activeClass) {
+  // se l'ultima immagine è active, ricomincia dalla prima
+  if ($(lastImage).hasClass(activeClass)) {
+    $(lastImage).removeClass(activeClass);
+    $(firstImage).addClass(activeClass);
+  // atrimenti vai a quella successiva
+  } else {
+   $(activeImage).removeClass(activeClass).next().addClass(activeClass);
+ }
+}
+
+
 // <- clicco sulla freccia a sinistra (torno indietro)
 $('.fa-angle-left').click(function() {
-  // se la prima immagine è active, ricomincia dall'ultima
-  if ($('img.first').hasClass('active')) {
-    $('img.first').removeClass('active');
-    $('img.last').addClass('active');
-    // altrimenti torna a quella precedente
-  } else {
-   $('img.active').removeClass('active').prev().addClass('active');
+  slideLeft('img.active', 'img.first', 'img.last', 'active');
  }
-});
+);
 
 // -> clicco sulla freccia a destra (vado avanti)
-$('.fa-angle-right').click(function () {
-  // se l'ultima immagine è active, ricomincia dalla prima
-  if ($('img.last').hasClass('active')) {
-    $('img.last').removeClass('active');
-    $('img.first').addClass('active');
-    // atrimenti vai a quella successiva
-  } else {
-    $('img.active').removeClass('active').next().addClass('active');
-  }
+$('.fa-angle-right').click(function() {
+  slideRight('img.active', 'img.first', 'img.last', 'active');
 });
 
 // per scorrere le immagini permettere anche l’uso delle frecce sinistra (37) e destra (39) della tastiera
 $(document).keydown(function(k) {
   if(k.keyCode == 37) { // left
-    if ($('img.first').hasClass('active')) {
-      $('img.first').removeClass('active');
-      $('img.last').addClass('active');
-    } else {
-     $('img.active').removeClass('active').prev().addClass('active');
-   }
+    slideLeft('img.active', 'img.first', 'img.last', 'active');
   }
   else if(k.keyCode == 39) { // right
-    if ($('img.last').hasClass('active')) {
-      $('img.last').removeClass('active');
-      $('img.first').addClass('active');
-    } else {
-      $('img.active').removeClass('active').next().addClass('active');
-    }
+    slideRight('img.active', 'img.first', 'img.last', 'active');
   }
 });
